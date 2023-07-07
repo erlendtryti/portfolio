2fba2494f0851f80e66453447335de30ea35588d
erlendtryti <erlendtl@icloud.com>
2023-07-07 03:25:25 +0200

    main page

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,9 +7,10 @@ const projectsCollection = defineCollection({
     image: image().refine((img) => img.width >= 1080, {
       message: "Cover image must be at least 1080 pixels wide!",
     }),
+    color: z.string().regex(/^[A-Fa-f0-9]+$/),
     date: z.string().datetime(),
     type: z.array(z.enum(["Design", "Development"])).nonempty(),
-    technology: z.array(z.enum(["Figma", "Astro", "Svelte", "Tailwind"])).nonempty(),
+    technology: z.array(z.enum(["Figma", "Astro", "Tailwind"])).nonempty(),
   }),
 });
 
